54a4dbacd39679adb72501773f8e940c7ed10c8e
godozi <tpdjq1900@gmail.com>
2025-07-01 14:21:48 +0900

    VR

---
+++ Assets/Scripts/ItemData.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemData : ScriptableObject
+{
+    public int id;
+    public string name;
+    public ItemCategory category;
+    public Sprite icon;
+    public GameObject prefab;
+}
+
+public enum ItemCategory
+{
+    Equipment,    // 장비
+    Fish,   // 물고기
+    Consumable  // 소비 아이템 ( 미끼, 스테미나 회복, 이벤트 아이템 등 )
+}
